00f7467638d9a6895bba07aed7dd5655d1d353cf
Ben Deane <ben@elbeno.com>
2016-04-02 11:50:49 -0700

    Move provided main() to testinator namespace
    Makes it easier to provide your own main() and also invoke testinator's
    main() function.

--- src/include/main.h
+++ src/include/main.h
@@ -3,133 +3,141 @@
 
 #pragma once
 
+namespace testinator
+{
+  int main(int argc, char** argv)
+  {
+    std::string outputterName;
+    std::string testName;
+    std::string suiteName;
+    testinator::RunParams p;
+    auto oflags = testinator::OF_COLOR|testinator::OF_QUIET_SUCCESS;
+
+    for (int i = 1; i < argc; ++i)
+    {
+      std::string s = argv[i];
+
+      {
+        std::string option = "--output=";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          outputterName = s.substr(option.size());
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--testName=";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          testName = s.substr(option.size());
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--suiteName=";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          suiteName = s.substr(option.size());
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--numChecks=";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          char* end;
+          p.m_numPropertyChecks = strtoul(s.substr(option.size()).c_str(), &end, 10);
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--seed=";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          char* end;
+          p.m_randomSeed = strtoul(s.substr(option.size()).c_str(), &end, 10);
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--alpha";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          p.m_flags |= testinator::RF_ALPHA_ORDER;
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--verbose";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          oflags &= ~(static_cast<std::underlying_type_t<testinator::OutputFlags>>(
+                          testinator::OF_QUIET_SUCCESS));
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--nocolor";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          oflags &= ~(static_cast<std::underlying_type_t<testinator::OutputFlags>>(
+                          testinator::OF_COLOR));
+          continue;
+        }
+      }
+
+      {
+        std::string option = "--help";
+        if (s.compare(0, option.size(), option) == 0)
+        {
+          std::cout << "Usage: testinator [OPTION]..." << std::endl
+                    << "Run all tests in randomized order by default." << std::endl
+                    << std::endl
+                    << "--testName=NAME    run only the named test" << std::endl
+                    << "--suiteName=NAME   run only the tests in the named suite" << std::endl
+                    << "--alpha            run tests in alphabetical order" << std::endl
+                    << "--output=FORMAT    use the specified output formatter, e.g. TAP" << std::endl
+                    << "--verbose          give verbose output (according to formatter)" << std::endl
+                    << "--nocolor          output without ANSI color codes (according to formatter)"
+                    << std::endl
+                    << "--numChecks=N      number of checks to use for property tests" << std::endl
+                    << "--seed=SEED        use SEED for property test randomization" << std::endl;
+          return 0;
+        }
+      }
+
+    }
+
+    std::unique_ptr<testinator::Outputter> op = testinator::MakeOutputter(
+        outputterName, static_cast<testinator::OutputFlags>(oflags));
+
+    testinator::Results rs;
+    if (!testName.empty())
+      rs = testinator::RunTest(testName, p, op.get());
+    else if (!suiteName.empty())
+      rs = testinator::RunSuite(suiteName, p, op.get());
+    else
+      rs = testinator::RunAllTests(p, op.get());
+
+    auto numPassed = count_if(rs.cbegin(), rs.cend(),
+                              [] (const testinator::Result& r) { return r.m_success; });
+    auto total = static_cast<decltype(numPassed)>(rs.size());
+    return static_cast<int>(total - numPassed);
+  }
+}
+
 #ifdef TESTINATOR_MAIN
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-  std::string outputterName;
-  std::string testName;
-  std::string suiteName;
-  testinator::RunParams p;
-  auto oflags = testinator::OF_COLOR|testinator::OF_QUIET_SUCCESS;
-
-  for (int i = 1; i < argc; ++i)
-  {
-    std::string s = argv[i];
-
-    {
-      std::string option = "--output=";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        outputterName = s.substr(option.size());
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--testName=";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        testName = s.substr(option.size());
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--suiteName=";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        suiteName = s.substr(option.size());
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--numChecks=";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        char* end;
-        p.m_numPropertyChecks = strtoul(s.substr(option.size()).c_str(), &end, 10);
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--seed=";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        char* end;
-        p.m_randomSeed = strtoul(s.substr(option.size()).c_str(), &end, 10);
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--alpha";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        p.m_flags |= testinator::RF_ALPHA_ORDER;
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--verbose";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        oflags &= ~(static_cast<std::underlying_type_t<testinator::OutputFlags>>(
-                        testinator::OF_QUIET_SUCCESS));
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--nocolor";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        oflags &= ~(static_cast<std::underlying_type_t<testinator::OutputFlags>>(
-                        testinator::OF_COLOR));
-        continue;
-      }
-    }
-
-    {
-      std::string option = "--help";
-      if (s.compare(0, option.size(), option) == 0)
-      {
-        std::cout << "Usage: testinator [OPTION]..." << std::endl
-                  << "Run all tests in randomized order by default." << std::endl
-                  << std::endl
-                  << "--testName=NAME    run only the named test" << std::endl
-                  << "--suiteName=NAME   run only the tests in the named suite" << std::endl
-                  << "--alpha            run tests in alphabetical order" << std::endl
-                  << "--output=FORMAT    use the specified output formatter, e.g. TAP" << std::endl
-                  << "--verbose          give verbose output (according to formatter)" << std::endl
-                  << "--nocolor          output without ANSI color codes (according to formatter)"
-                  << std::endl
-                  << "--numChecks=N      number of checks to use for property tests" << std::endl
-                  << "--seed=SEED        use SEED for property test randomization" << std::endl;
-        return 0;
-      }
-    }
-
-  }
-
-  std::unique_ptr<testinator::Outputter> op = testinator::MakeOutputter(
-      outputterName, static_cast<testinator::OutputFlags>(oflags));
-
-  testinator::Results rs;
-  if (!testName.empty())
-    rs = testinator::RunTest(testName, p, op.get());
-  else if (!suiteName.empty())
-    rs = testinator::RunSuite(suiteName, p, op.get());
-  else
-    rs = testinator::RunAllTests(p, op.get());
-
-  auto numPassed = count_if(rs.cbegin(), rs.cend(),
-                            [] (const testinator::Result& r) { return r.m_success; });
-  auto total = static_cast<decltype(numPassed)>(rs.size());
-  return static_cast<int>(total - numPassed);
+  return testinator::main(argc, argv);
 }
 
 #endif
